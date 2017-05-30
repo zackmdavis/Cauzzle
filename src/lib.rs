555fbd3ff527c37174aec25957a05d849ca302cf
Zack M. Davis <code@zackmdavis.net>
2017-05-30 16:57:04 -0700

    sketching out structural causal models, led by matching pennies example

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,135 @@
+#![allow(dead_code)] // for the moment
+
+extern crate petgraph;
+extern crate rand;
+
 use std::cmp;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::fmt;
-use std::mem;
 use std::rc::{Rc, Weak};
 
+use petgraph::{Directed, Direction, Graph};
+use petgraph::graph::{NodeIndex, EdgeIndex};
+use petgraph::visit::Topo;
+
+
+#[derive(Copy, Clone, Debug)]
+enum VariableState {
+    Categorical(usize),
+    Integer(isize),
+    Float(f64)
+}
+
+struct Variable {
+    identifier: String,
+    state: Option<VariableState>,
+    structure: Box<Fn(&[VariableState]) -> VariableState>
+}
+
+impl Variable {
+    fn new<I: ToString>(identifier: I, state: Option<VariableState>,
+                        structure: Box<Fn(&[VariableState]) -> VariableState>)
+                        -> Self {
+        Variable { identifier: identifier.to_string(),
+                   state: state,
+                   structure: structure }
+    }
+}
+
+struct StructuralCausalModel(Graph<Variable, (), Directed>);
+
+impl StructuralCausalModel {
+    fn new() -> Self {
+        StructuralCausalModel(Graph::new())
+    }
+
+    fn add_variable(&mut self, variable: Variable) -> NodeIndex {
+        self.0.add_node(variable)
+    }
+
+    fn add_arrow(&mut self,
+                 parent_index: NodeIndex,
+                 child_index: NodeIndex) -> EdgeIndex {
+        self.0.add_edge(parent_index, child_index, ())
+    }
+
+    fn parent_states(&self, index: NodeIndex) -> Vec<VariableState> {
+        self.0.neighbors_directed(index, Direction::Incoming)
+            .map(|j| self.0.node_weight(j).expect("parent exists")
+                 .state.expect("parent state must be set"))
+            .collect()
+    }
+
+    fn evaluate_variable(&mut self, index: NodeIndex) {
+        let parent_states = self.parent_states(index);
+        let state = (self.0.node_weight(index)
+                     .expect("variable should exist").structure)(&parent_states);
+        self.0.node_weight_mut(index)
+            .expect("variable should exist").state = Some(state);
+    }
+
+    fn evaluate(&mut self) {
+        let mut topological_visitor = Topo::new(&self.0);
+        while let Some(next_index) = topological_visitor.next(&self.0) {
+            self.evaluate_variable(next_index);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::random;
+
+    use super::{StructuralCausalModel, Variable, VariableState};
+
+
+    const HEADS: VariableState = VariableState::Categorical(0);
+    const TAILS: VariableState = VariableState::Categorical(1);
+
+    fn coinflip(_dummy: &[VariableState]) -> VariableState {
+        if random::<f64>() < 0.5 {
+            HEADS
+        } else {
+            TAILS
+        }
+    }
+
+    fn parity(parents: &[VariableState]) -> VariableState {
+        let mut parity_bit = 0;
+        for parent in parents {
+            match parent {
+                &VariableState::Categorical(i) => {
+                    parity_bit += i % 2;
+                }
+                s @ _ => panic!("unexpectedly non-categorical \
+                                 variable state {:?}", s)
+            };
+        }
+        VariableState::Categorical(parity_bit)
+    }
+
+    #[test]
+    fn matching_pennies() {
+        // Let's start with a minimal example. Say, a collider: two coinflips
+        // jointly cause a variable indicating their parity.
+        let one_penny = Variable::new("penny A", None, Box::new(coinflip));
+        let another_penny = Variable::new("penny B", None, Box::new(coinflip));
+        let matching = Variable::new("matching", None, Box::new(parity));
+
+        // set up the graph
+        let mut model = StructuralCausalModel::new();
+        let one_penny_index = model.add_variable(one_penny);
+        let another_penny_index = model.add_variable(another_penny);
+        let matching_index = model.add_variable(matching);
+        model.add_arrow(one_penny_index, matching_index);
+        model.add_arrow(another_penny_index, matching_index);
+
+        model.evaluate();
+    }
+}
+
+// March 2016 draft follows—
 
 struct NaiveVariable {
     identifier: String,
@@ -279,7 +404,7 @@ impl fmt::Display for Path {
 }
 
 #[cfg(test)]
-mod test {
+mod naive_tests {
     use super::{Network, NaiveVariable};
 
     // the example network from _Causality_ §1.2
